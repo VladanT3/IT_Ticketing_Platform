e8b2d9e2a27b3de46a45a5f55cf30a5c1dbe1cae
VladanT3 <vladt.t33@gmail.com>
2024-07-13 18:27:29 +0200

    added a basic login form to test login

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/VladanT3/IT_Ticketing_Platform
 go 1.22.5
 
 require (
-	github.com/a-h/templ v0.2.747 // indirect
-	github.com/go-chi/chi/v5 v5.1.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/a-h/templ v0.2.747
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/joho/godotenv v1.5.1
 )
--- views/layouts/base_templ.go
+++ views/layouts/base_templ.go
@@ -26,7 +26,15 @@ func Base() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><title>IT Tickets</title><link rel=\"icon\" type=\"image/x-icon\" href=\"/public/favicon.ico\"><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" href=\"/public/styles.css\"><script src=\"https://unpkg.com/htmx.org@2.0.1\" integrity=\"sha384-QWGpdj554B4ETpJJC9z+ZHJcA/i59TyjxEPXiiUgN2WmTyV5OEZWCD6gQhgkdpB/\" crossorigin=\"anonymous\"></script></head><body>Hi</body></html>")
+		templ_7745c5c3_Err = templ.WriteWatchModeString(templ_7745c5c3_Buffer, 1)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templ_7745c5c3_Var1.Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templ.WriteWatchModeString(templ_7745c5c3_Buffer, 2)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
