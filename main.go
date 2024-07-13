e6ba23daa3d23153caf03260520117c3853e9570
VladanT3 <vladt.t33@gmail.com>
2024-07-13 18:34:24 +0200

    added db connection

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,12 +10,25 @@ import (
 	handlers "github.com/VladanT3/IT_Ticketing_Platform/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading env variables")
+		log.Fatal("Error loading env variables: ", err)
+	}
+
+	dbConn := os.Getenv("DB_CONN")
+	db, err := sql.Open("postgres", dbConn)
+	if err != nil {
+		log.Fatal("Unable to connect to database: ", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Unable to ping database: ", err)
 	}
 
 	router := chi.NewMux()
