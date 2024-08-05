6c9c35dd06c1d05e957033918f69b5db643abbc7
VladanT3 <vladt.t33@gmail.com>
2024-08-05 21:07:41 +0200

    added layout when user is lacking certain credentials

--- handlers/showTeamTicketsHandler.go
+++ handlers/showTeamTicketsHandler.go
@@ -3,9 +3,14 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/VladanT3/IT_Ticketing_Platform/views/layouts"
 	"github.com/VladanT3/IT_Ticketing_Platform/views/tickets"
 )
 
 func ShowTeamTicketsHandler(w http.ResponseWriter, r *http.Request) error {
+	if LoggedInUserType != "manager" {
+		return Render(w, r, layouts.AccessDenied(LoggedInUser, LoggedInUserType, "Access Denied: Lack of managerial credentials!"))
+	}
+
 	return Render(w, r, tickets.TicketSearch(LoggedInUser, LoggedInUserType, "Team Tickets"))
 }
